fbbad5332fc953e65c5bdeeb24759ed3ca3a67b1
Artem Innokentiev <artinnok@mail.ru>
2016-10-04 22:50:04 +0300

    REMOVE useless files

--- habr-api/static/src/index.js
+++
@@ -1,8 +0,0 @@
-// JS
-function requireAll(requireContext) {
-  return requireContext.keys().map(requireContext);
-}
-var modules = requireAll(require.context('./blocks', true, /\.js$/));
-
-// SCSS
-require.context('./blocks', true, /\.scss$/);
