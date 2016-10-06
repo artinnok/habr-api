10a54bc0e886bb0c62b0940933bc114cd8a5e32b
Artem Innokentiev <artinnok@users.noreply.github.com>
2016-10-06 11:34:40 +0300

    FIX

--- habr-api/core/views.py
+++ habr-api/core/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from core.tasks import parse
+from core.tasks import parse_habr
 
 
 class IndexView(TemplateView):
     template_name = 'core/index.html'
 
     def post(self, request, *args, **kwargs):
-        parse.delay()
+        parse_habr.delay()
 
 
 def error404(request):
